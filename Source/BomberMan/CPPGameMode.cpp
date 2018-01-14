cb467dfb0ee94c21d972d12c379f1229c3eb9dce
SebastianLuo <hadis-zero@126.com>
2018-01-14 15:30:59 +0800

    add procedural generated maps functionality

--- Source/BomberMan/CPPGameMode.cpp
+++ Source/BomberMan/CPPGameMode.cpp
@@ -6,3 +6,117 @@
 
 
 
+
+ACPPGameMode::ACPPGameMode()
+{
+	ResetRate();
+
+	m_iSceneWidth = 30;
+	m_iSceneHeight = 30;
+}
+
+void ACPPGameMode::ResetRate()
+{
+	m_iDRate = 60;
+	m_iUDRate = 20;
+}
+
+void ACPPGameMode::StartPlay()
+{
+	m_fSceneBaseX = -(m_iSceneWidth / 2.0f * 100.0f);
+	m_fSceneBaseY = -(m_iSceneHeight / 2.0f * 100.0f);
+
+	Super::StartPlay();
+}
+
+TArray<FSingleElementInfo> ACPPGameMode::GenerateMap(int iWidth, int iHeight)
+{
+	if (m_iDRate + m_iUDRate > 100)
+	{
+		ResetRate();
+	}
+
+	const int iAbsEmptyAreaWidth = 2;
+	const int iAbsEmptyAreaHeight = 2;
+
+	TArray<FSingleElementInfo> eaMap;
+
+	if (iWidth < 4 || iHeight < 4)
+		return eaMap;
+
+	for (int i = 0; i < iHeight; ++i)
+	{
+		for (int j = 0; j < iWidth; ++j)
+		{
+			FSingleElementInfo oSingleElementInfo;
+			if ((i < iAbsEmptyAreaWidth && j < iAbsEmptyAreaHeight)
+				|| (iHeight - 1 - i) < iAbsEmptyAreaWidth && (iWidth - 1 - j) < iAbsEmptyAreaHeight)
+			{
+				oSingleElementInfo.m_eElementType = eET_None;
+			}
+			else
+			{
+				int iRandRate = rand() % 100;
+				if (iRandRate < m_iUDRate)
+				{
+					oSingleElementInfo.m_eElementType = eET_UD;
+				}
+				else if (iRandRate < (m_iUDRate + m_iDRate))
+				{
+					oSingleElementInfo.m_eElementType = eET_D;
+				}
+				else
+				{
+					oSingleElementInfo.m_eElementType = eET_None;
+				}
+			}
+
+			int iCurElementIndex = eaMap.Num();
+			
+			oSingleElementInfo.m_fX = m_fSceneBaseX + iCurElementIndex % iWidth * 100 + 50.0f;
+			oSingleElementInfo.m_fY = m_fSceneBaseY + iCurElementIndex / iWidth * 100 + 50.0f;
+
+			eaMap.Add(oSingleElementInfo);
+		}
+	}
+
+ 	return eaMap;
+}
+
+void ACPPGameMode::SetFloorWallScaleAndLocation(EFloorWallType eType, AActor* pActor)
+{
+	switch (eType)
+	{
+	case eFWT_Floor:
+	{
+		pActor->SetActorScale3D(FVector(m_iSceneWidth, m_iSceneHeight, 1.0f));
+		break;
+	}
+	case eFWT_Top:
+	{
+		pActor->SetActorScale3D(FVector(m_iSceneWidth, 1.0f, 1.0f));
+		pActor->SetActorLocation(FVector(0.0f, float(m_iSceneHeight) / 2.0f * (-100.0f) - 50.0f, 50.0f));
+		break;
+	}
+	case eFWT_Bottom:
+	{
+		pActor->SetActorScale3D(FVector(m_iSceneWidth, 1, 1));
+		pActor->SetActorLocation(FVector(0.0f, float(m_iSceneHeight) / 2.0f * (100.0f) + 50.0f, 50.0f));
+		break;
+	}
+	case eFWT_Left:
+	{
+		pActor->SetActorScale3D(FVector(m_iSceneHeight, 1, 1));
+		pActor->SetActorLocation(FVector(float(m_iSceneWidth) / 2.0f * (-100.0f) - 50.0f, 0.0f, 50.0f));
+		break;
+	}
+	case eFWT_Right:
+	{
+		pActor->SetActorScale3D(FVector(m_iSceneHeight, 1, 1));
+		pActor->SetActorLocation(FVector(float(m_iSceneWidth) / 2.0f * (100.0f) + 50.0f, 0.0f, 50.0f));
+		break;
+	}
+	default:
+		break;
+	}
+}
--- Source/BomberMan/CPPGameMode.h
+++ Source/BomberMan/CPPGameMode.h
@@ -5,6 +5,49 @@
 #include "GameFramework/GameMode.h"
 #include "CPPGameMode.generated.h"
 
+
+UENUM()
+enum EElementType
+{
+	eET_None,
+
+	eET_UD,
+	eET_D,
+};
+
+UENUM()
+enum EFloorWallType
+{
+	eFWT_Floor,
+
+	eFWT_Top,
+	eFWT_Bottom,
+	eFWT_Left,
+	eFWT_Right
+};
+
+USTRUCT()
+struct FSingleElementInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = BuildScene)
+	TEnumAsByte<EElementType>	m_eElementType;
+	
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = BuildScene)
+	float			m_fX;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = BuildScene)
+	float			m_fY;
+
+	FSingleElementInfo()
+	{
+		m_eElementType = eET_None;
+		m_fX = 0.0f;
+		m_fY = 0.0f;
+	}
+};
+
 /**
  * 
  */
@@ -13,7 +56,40 @@ class BOMBERMAN_API ACPPGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+	ACPPGameMode();
+
+	UFUNCTION(BlueprintCallable, Category = GamePlay)
+		TArray<FSingleElementInfo> GenerateMap(int iWidth, int iHeight);
+
+	UFUNCTION(BlueprintCallable, Category = GamePlay)
+		void SetFloorWallScaleAndLocation(EFloorWallType eType, AActor* pActor);
+
+	void ResetRate();
+
+public:
+	virtual void StartPlay() override;
+
+	/** please don't modify m_iSceneWidth and m_iSceneHeight, unless you know what you're doint */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Cfg)
+		int				m_iSceneWidth;
+
+	/** please don't modify m_iSceneWidth and m_iSceneHeight, unless you know what you're doint */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Cfg)
+		int				m_iSceneHeight;
+
+	/** m_iUDRate + m_iDRate should be <= 100 */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Cfg)
+		int				m_iUDRate;
+
+	/** m_iUDRate + m_iDRate should be <= 100 */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Cfg)
+		int				m_iDRate;
+
+	/** top left x */
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = Cfg)
+		float			m_fSceneBaseX;
+
+	/** top left y */
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = Cfg)
+		float			m_fSceneBaseY;
 };
