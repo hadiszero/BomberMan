beafd5acbc1efaf63a9297b23784a5efc2542336
SebastianLuo <hadis-zero@126.com>
2018-01-14 15:27:04 +0800

    Basic project setting. Input setting and handling. Add both cpp,bp gamemode and gameinstance

---
+++ Source/BomberMan/CPPGameInstance.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "CPPGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN_API UCPPGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
